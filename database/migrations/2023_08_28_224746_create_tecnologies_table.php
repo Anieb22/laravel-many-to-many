7ae480a93350d74028aac5febd961555f9afa850
Anieb22 <anielloverdino@gmail.com>
2023-08-29 00:54:19 +0200

    aggiunto il metodo many to many

--- database/migrations/2023_08_28_224746_create_tecnologies_table.php
+++ database/migrations/2023_08_28_224746_create_tecnologies_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('tecnologies', function (Blueprint $table) {
             $table->id();
+            $table->string('tecnology');
             $table->timestamps();
         });
     }
